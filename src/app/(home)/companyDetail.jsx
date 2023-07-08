ac0f5fd3a3715f9a3c4373de0883885f4f69a35c
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-07-08 13:21:16 +0600

    company details in home page added

--- src/app/(home)/companyDetail.jsx
+++ src/app/(home)/companyDetail.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { VscServerProcess } from 'react-icons/vsc';
 import { BiRightArrowCircle} from 'react-icons/bi';
 import { BsMicrosoftTeams } from 'react-icons/bs';
+import { MdRoundaboutRight } from 'react-icons/md';
 
 
 
@@ -12,7 +13,7 @@ const CompanyDetail = () => {
   return (
     <div  className="company-details px-24 z-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {/* our process div */}
-      <div className="bg-white h-[80%] my-[10%] p-5 mx-10 hover:border-b-8 hover:border-b-orange-500">
+      <div className="bg-white h-[85%] my-[7%] p-5 mx-10 hover:border-b-8 hover:border-b-orange-500 relative">
         <VscServerProcess className=" mx-auto w-10 h-10 mb-2 text-orange-500"></VscServerProcess>
         <h2 className="text-2xl font-bold mb-4 text-center">
           We're challenging the conventional norms of a typical events company.
@@ -20,15 +21,15 @@ const CompanyDetail = () => {
         <p className="text-justify">we are actively pushing against the established norms and expectations associated with traditional events companies. We are daring to think differently, innovating, and breaking away from the conventional approach to deliver unique and exceptional event experiences. Our aim is to redefine the industry by offering fresh perspectives, creative solutions, and transformative events that go beyond expectations.</p>
 
         
-       <div className="flex mt-3 items-center justify-between">
-       <p className="text-xl font-semibold text-orange-500">Learn about Our Process 
+       <div className="flex items-center justify-between absolute bottom-8">
+       <p className="text-xl font-semibold ">Get to Know Our Methodology
           </p> 
           <BiRightArrowCircle className="w-8 h-8 text-orange-500" />
        </div>
       </div>
 
       {/* out team div */}
-      <div className="bg-white h-[80%] my-[10%] p-5 mx-10 hover:border-b-8 hover:border-b-orange-500">
+      <div className="bg-white h-[85%] my-[7%] p-5 mx-10 hover:border-b-8 hover:border-b-orange-500 relative">
         <BsMicrosoftTeams className=" mx-auto w-10 h-10 mb-2 text-orange-500"></BsMicrosoftTeams>
         <h2 className="text-2xl font-bold mb-4 text-center">
         Your Event Dreams, Our Team's Expertise.
@@ -36,8 +37,23 @@ const CompanyDetail = () => {
         <p className="text-justify">Our team is passionate about creating exceptional events that exceed expectations. We work closely with our clients to understand their vision, preferences, and objectives, ensuring that every detail is carefully curated to reflect their individual style and desires. From corporate gatherings to weddings, conferences to private celebrations, our team's expertise spans across a diverse range of event types.</p>
 
         
-       <div className="flex mt-3 items-center justify-between">
-       <p className="text-xl font-semibold text-orange-500">Learn about Our Process 
+       <div className="flex mt-6 items-center justify-between absolute bottom-8">
+       <p className="text-xl font-semibold">Know more about our team 
+          </p> 
+          <BiRightArrowCircle className="w-8 h-8 text-orange-500" />
+       </div>
+      </div>
+      {/* about us div */}
+      <div className="bg-white h-[85%] my-[7%] p-5 mx-10 hover:border-b-8 hover:border-b-orange-500 relative">
+        <MdRoundaboutRight className=" mx-auto w-10 h-10 mb-2 text-orange-500"></MdRoundaboutRight>
+        <h2 className="text-2xl font-bold mb-4 text-center">
+        Unveiling Our Story: Crafting Unforgettable Experiences.
+        </h2>
+        <p className="text-justify">We believe that every event has a unique story waiting to unfold. From intimate gatherings to grand celebrations, we carefully listen to our clients' desires, immerse ourselves in their aspirations, and blend our creativity with meticulous attention to detail. This harmonious fusion allows us to transform visions into reality, weaving together the elements that make each event extraordinary.</p>
+
+        
+       <div className="flex mt-6 items-center justify-between absolute bottom-8">
+       <p className="text-xl font-semibold ">Gain Insight into what we are 
           </p> 
           <BiRightArrowCircle className="w-8 h-8 text-orange-500" />
        </div>
